fe9b6fa5679c94d801705f525203b97783ec0f01
Sanjib Kumar Sah <35219515+s4nju@users.noreply.github.com>
2020-10-21 20:53:01 +0530

    button added

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonClicked: false
+    };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  handleClick() {
+    console.log("Clicked");
+    this.setState({ buttonClicked: false });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        <button id="click" onClick={this.handleClick()}></button>
+        {this.state.buttonClicked ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : null}
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
